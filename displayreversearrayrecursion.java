1cd0615b47b42f4ba39c538535a982900a8f028e
Daksh Chhabra <115346924+Daksh7112003@users.noreply.github.com>
2023-07-18 20:59:55 +0530

    Add files via upload

---
+++ displayreversearrayrecursion.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+
+public class displayreversearrayrecursion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] =  new int[n];
+        for(int i =0 ; i<n ;i++){
+            arr[i]= sc.nextInt();
+
+
+        }
+        dar(arr,0);
+
+    }
+
+    public static void dar(int arr[], int idx){
+        if(idx==arr.length){
+            return;
+
+        }
+        dar(arr, idx+1);
+        System.out.println(arr[idx]);
+    }
+}
