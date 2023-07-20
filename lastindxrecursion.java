976081da741f4701eef1e6d4ae252c56a20a45a0
Daksh Chhabra <115346924+Daksh7112003@users.noreply.github.com>
2023-07-21 00:24:44 +0530

    Add files via upload

---
+++ lastindxrecursion.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class lastindxrecursion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[]= new int[n];
+        for(int i = 0 ; i< n ; i++){
+            arr[i]= sc.nextInt();
+
+        }
+int d =sc.nextInt();
+int lii= lastindex(arr, 0, d);
+System.out.println(lii);
+    }
+
+public static int lastindex(int arr[] , int idx, int x ){
+    if(idx==arr.length){
+        return -1 ;
+
+    }
+    int li = lastindex(arr, idx+1, x);
+    if(li==-1){
+        if(arr[idx]==x){
+            return idx;
+        }
+        else{
+            return -1;
+        }
+    }
+    else{
+            return li;
+
+        
+    }
+}
+}
+
