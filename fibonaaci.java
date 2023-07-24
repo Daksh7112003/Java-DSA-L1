4760a2f2cf027d5faac33918672b5dd0156fe58d
Daksh <115346924+Daksh7112003@users.noreply.github.com>
2023-07-24 23:01:47 +0530

    Add files via upload

---
+++ fibonaaci.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class fibonaaci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int a = 0 ;
+        int b =1 ;
+
+        for(int i =0 ; i<n ; i++){
+            System.out.println(a);
+            int c= a+b;
+            a=b;
+            b=c;
+            
+
+            
+
+
+            
+
+        }
+    
+        
+    }
+    
+
+}
