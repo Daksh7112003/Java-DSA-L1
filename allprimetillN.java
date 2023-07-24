d7f4a5aa0c53783ee74b71f6cc50922621b4e1da
Daksh <115346924+Daksh7112003@users.noreply.github.com>
2023-07-24 21:38:08 +0530

    Add files via upload

---
+++ allprimetillN.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class allprimetillN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int low = sc.nextInt();
+        int high = sc.nextInt();
+        for(int i =low ; i<=high ; i++){
+          int count= 0; 
+        for(int div =2 ; div*div<=i ;div++){
+            if(i%div==0){
+                count++;
+                break;
+
+
+
+
+
+            }
+        }
+
+
+            
+            if(count==0){
+                System.out.println(i);
+            }
+
+     
+    }
+        }
+    }
+
