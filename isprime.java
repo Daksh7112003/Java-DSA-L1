b65d40be493960175bd6ab79fc750dd3e3fdbc2a
Daksh <115346924+Daksh7112003@users.noreply.github.com>
2023-07-24 21:14:21 +0530

    Update isprime.java

--- isprime.java
+++ isprime.java
@@ -1,28 +1,28 @@
-// import java.util.*;
-// public class isprime {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-//         int n = sc.nextInt();
-//         int count = 0 ; 
+import java.util.*;
+public class isprime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0 ; 
 
-//         for(int i = 2;i<n; i++ ){
-//             if(n%i==0){
-//             count++;
-//             break;
+        for(int i = 2;i<n; i++ ){
+            if(n%i==0){
+            count++;
+            break;
 
             
-//             }
-//         }
-//             if(count>0){
-//                 System.out.println("not prime");
-//             }
-//             else{
-//                 System.out.println("prime");
-//             }
+            }
+        }
+            if(count>0){
+                System.out.println("not prime");
+            }
+            else{
+                System.out.println("prime");
+            }
 
         
-//     }
-// }
+    }
+}
  // in the above approach the time complexity is higher..
  // so easy approach..
 
