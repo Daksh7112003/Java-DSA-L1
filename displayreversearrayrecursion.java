93410056623e10a65b97da20ca10d4e53447aa24
Daksh <115346924+Daksh7112003@users.noreply.github.com>
2023-07-24 12:51:47 +0530

    Update displayreversearrayrecursion.java

--- displayreversearrayrecursion.java
+++ displayreversearrayrecursion.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 public class displayreversearrayrecursion {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
         int arr[] =  new int[n];
         for(int i =0 ; i<n ;i++){
             arr[i]= sc.nextInt();
