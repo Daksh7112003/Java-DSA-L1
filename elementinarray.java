8ac41778fed1f22f33ad4c3c7e1c25b2ce6e0f75
Daksh <115346924+Daksh7112003@users.noreply.github.com>
2023-07-24 20:21:54 +0530

    Update elementinarray.java

--- elementinarray.java
+++ elementinarray.java
@@ -2,8 +2,8 @@ import java.util.*;
 public class elementinarray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int a[] = new int[n];
+        int x = sc.nextInt();
+        int a[] = new int[x];
         for(int i = 0 ;i <a.length; i++){
             a[i]= sc.nextInt();
 
