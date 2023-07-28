f8cfe64e85e8ff1f07d7b933f963ce25790af474
Daksh <115346924+Daksh7112003@users.noreply.github.com>
2023-07-28 22:43:30 +0530

    Add files via upload

---
+++ getsubsequence.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class getsubsequence {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String str = "hello";
+    for(int i =0 ; i<str.length(); i++){
+        char ch= str.charAt(i);
+        System.out.println(ch);
+
+    }
+    String ss= str.substring(1,3);
+    System.out.println(ss);
+    
+}
+    
+}
