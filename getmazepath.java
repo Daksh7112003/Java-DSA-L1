be6cfa7bb1066c77fb73b7ecb5958eee5493be9b
Daksh <115346924+Daksh7112003@users.noreply.github.com>
2023-08-27 23:08:44 +0530

    Add files via upload

---
+++ getmazepath.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class getmazepath {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        ArrayList<String> paths = getMazePaths(1, 1, n, m);
+        System.out.println(paths);
+    }
+
+    public static ArrayList<String> getMazePaths(int sr, int sc, int dr, int dc) {
+        if (sr == dr && sc == dc) {
+            ArrayList<String> bres = new ArrayList<>();
+            bres.add("");
+            return bres;
+        }
+
+        ArrayList<String> hpaths = new ArrayList<>();
+        ArrayList<String> vpaths = new ArrayList<>();
+
+        if (sc < dc) {
+            hpaths = getMazePaths(sr, sc + 1, dr, dc);
+        }
+        if (sr < dr) {
+            vpaths = getMazePaths(sr + 1, sc, dr, dc);
+        }
+
+        ArrayList<String> paths = new ArrayList<>();
+
+        for (String hpath : hpaths) {
+            paths.add("h" + hpath);
+        }
+        for (String vpath : vpaths) {
+            paths.add("v" + vpath);
+        }
+
+        return paths;
+    }
+}
