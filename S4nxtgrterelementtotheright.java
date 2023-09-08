e8b09d201e8e7058105f71e74944fa38124bea95
Daksh <115346924+Daksh7112003@users.noreply.github.com>
2023-09-08 17:41:02 +0530

    Update S4nxtgrterelementtotheright.java

--- S4nxtgrterelementtotheright.java
+++ S4nxtgrterelementtotheright.java
@@ -1,26 +1,61 @@
-import java.util.*;
-
+ import java.util.*;
+import java.io.*;
 public class S4nxtgrterelementtotheright{
-    public static int[] nextGreaterElement(int[] nums) {
-        int[] result = new int[nums.length];
-        Arrays.fill(result, -1); // Initialize all elements to -1.
+ public static void display(int[] a){
+    StringBuilder sb = new StringBuilder();
 
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < nums.length; i++) {
-            while (!stack.isEmpty() && nums[i] > nums[stack.peek()]) {
-                int idx = stack.pop();
-                result[idx] = nums[i];
-            }
-            stack.push(i);
-        }
-        return result;
+    for(int val: a){
+      sb.append(val + "\n");
     }
- 
-    public static void main(String[] args) {
-        int[] nums = {4, 2, 10, 7, 5, 8};
-        int[] nextGreater = nextGreaterElement(nums);
+    System.out.println(sb);
+  }
 
-        System.out.println("Original Array: " + Arrays.toString(nums));
-        System.out.println("Next Greater Elements: " + Arrays.toString(nextGreater));
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] a = new int[n];
+    for(int i = 0; i < n; i++){
+       a[i] = Integer.parseInt(br.readLine());
     }
+
+    int[] nge = solve(a);
+    display(nge);
+ }
+
+ public static int[] solve(int[] arr){
+   // solve
+int[]nge = new int[arr.length];
+Stack<Integer> st= new Stack<>();
+st.push(arr[arr.length-1]);
+nge[arr.length-1]=-1;
+for(int i=arr.length-2;i>=0;i--){
+    while(st.size()>0&& arr[i]>st.peek()){
+        st.pop();
+
+    }
+    if(st.size()==0){
+        nge[i]=-1;
+
+
+    }
+    else{
+nge[i]=st.peek();
+
+
+    
+
+
 }
+st.push(arr[i]);
+
+} 
+
+
+
+
+
+   return nge;
+
+ }
+
