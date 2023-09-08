049931ca4b68f6aee2300547c6c2663fdd98e683
Daksh <115346924+Daksh7112003@users.noreply.github.com>
2023-09-08 18:12:28 +0530

    Update and rename S6Slidingwindowmax.java to S7Slidingwindowmax.java

--- S7Slidingwindowmax.java
+++ S7Slidingwindowmax.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class S6Slidingwindowmax {
+public class S7Slidingwindowmax {
   
 public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
