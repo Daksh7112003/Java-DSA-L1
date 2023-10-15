8209396ed2031a383a4936e3b6b902bf921b06c7
Daksh <115346924+Daksh7112003@users.noreply.github.com>
2023-10-15 22:25:19 +0530

    Add files via upload

---
+++ DP4ClimbingStairsMINMOVES.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class DP4ClimbingStairsMINMOVES {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int arr[]= new int[n];
+
+    for(int i=0; i<arr.length;i++){
+        arr[i]= sc.nextInt();
+
+
+    }
+    Integer[]dp= new Integer[n+1];
+    dp[n]=0;
+
+    for(int i= n-1; i>=0;i--){
+        if(arr[i]>0){
+            int min = Integer.MAX_VALUE;
+            for(int j=1 ; j<=arr[i] && i+j<dp.length;j++){
+                if(dp[i+j]!=null){
+                    min=Math.min(min,dp[i+j]);
+                }
+            }
+            if(min!=Integer.MAX_VALUE ){
+                dp[i]=min+1;
+
+            }
+
+
+        }
+        
+    }
+    System.out.println(dp[0]);
+
+    }
+}
