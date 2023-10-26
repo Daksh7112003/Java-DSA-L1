55ccab0b28a1f6e8e7024051a01c71f0cc936f0b
Daksh <115346924+Daksh7112003@users.noreply.github.com>
2023-10-26 19:36:44 +0530

    Add files via upload

---
+++ DP10a01Knapsack.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class DP10a01Knapsack{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int vals[]= new int[n];
+        int[]wts = new int[n];
+        for(int i = 0;  i<vals.length ; i++){
+            vals[i]=sc.nextInt();
+
+
+        }
+        for(int i = 0 ; i<wts.length;i++){
+            wts[i]=sc.nextInt();
+
+        }
+
+         
+        int cap = sc.nextInt();
+        int[][]dp = new int[n+1][cap+1];
+        for(int i =1  ; i<dp.length ; i++){
+            for(int  j = 1  ; j<dp[0].length ; j++){
+                if(j>=wts[i-1]){
+                    int rcap  = j - wts[i-1];
+                    if(dp[i-1][rcap] +vals[i-1] >dp[i-1][j]){
+                        dp[i][j]=dp[i-1][rcap] +vals[i-1];
+
+                    }
+                    else{
+                        dp[i][j]= dp[i-1][j];
+
+                    }
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+
+                }
+
+            }
+
+        }
+        System.out.println(dp[n][cap]);
+
+    
+    }
+}
